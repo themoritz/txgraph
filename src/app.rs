9789ddfe87d2537a6900ce4e850fc98db9a5c268
Moritz Drexl <mdrexl@fastmail.fm>
2023-03-15 22:10:37 +0100

    cleanup

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use eframe::epaint::PathShape;
-use egui::{Pos2, Painter, Color32, Stroke, Vec2, Rect, Rounding};
+use egui::{Color32, Painter, Pos2, Rect, Rounding, Stroke, Vec2};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -7,7 +7,7 @@ use egui::{Pos2, Painter, Color32, Stroke, Vec2, Rect, Rounding};
 pub struct App {
     from: Pos2,
     to: Pos2,
-    height: f32
+    height: f32,
 }
 
 impl Default for App {
@@ -15,7 +15,7 @@ impl Default for App {
         Self {
             from: Pos2::new(50.0, 50.0),
             to: Pos2::new(250.0, 150.0),
-            height: 50.0
+            height: 50.0,
         }
     }
 }
@@ -23,13 +23,11 @@ impl Default for App {
 impl App {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-
-
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
         cc.egui_ctx.set_visuals(egui::Visuals::light());
 
-        crate::bitcoin::get();
+        crate::bitcoin::get().unwrap();
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
@@ -47,23 +45,37 @@ impl eframe::App for App {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-
             let size = Vec2::new(10.0, self.height);
-            let (response, painter) = ui.allocate_painter(ui.available_size_before_wrap(), egui::Sense::hover());
+            let (response, painter) =
+                ui.allocate_painter(ui.available_size_before_wrap(), egui::Sense::hover());
 
             let from_rect = Rect::from_min_size(self.from - Vec2::new(10.0, 0.0), size);
             let from_response = ui.interact(from_rect, response.id.with(1), egui::Sense::drag());
             self.from += from_response.drag_delta();
             let color = ui.style().interact(&from_response).bg_fill;
-            painter.rect(from_rect.translate(from_response.drag_delta()), Rounding::none(), color, Stroke::NONE);
+            painter.rect(
+                from_rect.translate(from_response.drag_delta()),
+                Rounding::none(),
+                color,
+                Stroke::NONE,
+            );
 
             let to_rect = Rect::from_min_size(self.to, size);
             let to_response = ui.interact(to_rect, response.id.with(2), egui::Sense::drag());
             self.to += to_response.drag_delta();
             let color = ui.style().interact(&to_response).bg_fill;
-            painter.rect(to_rect.translate(to_response.drag_delta()), Rounding::none(), color, Stroke::NONE);
+            painter.rect(
+                to_rect.translate(to_response.drag_delta()),
+                Rounding::none(),
+                color,
+                Stroke::NONE,
+            );
 
-            let edge = Edge { height: self.height, from: self.from, to: self.to };
+            let edge = Edge {
+                height: self.height,
+                from: self.from,
+                to: self.to,
+            };
             edge.ui_content(&painter);
         });
 
@@ -79,7 +91,7 @@ impl eframe::App for App {
 struct Edge {
     height: f32,
     from: Pos2,
-    to: Pos2
+    to: Pos2,
 }
 
 impl Edge {
@@ -91,7 +103,8 @@ impl Edge {
 
         let mut last = self.from;
 
-        let steps = (((self.to.x - self.from.x).abs() + (self.to.y - self.from.y).abs()) / 4.0) as i32;
+        let steps =
+            (((self.to.x - self.from.x).abs() + (self.to.y - self.from.y).abs()) / 4.0) as i32;
 
         for n in 1..=steps {
             let t = n as f32 / steps as f32;
@@ -99,15 +112,20 @@ impl Edge {
             let c2 = c * c;
             let t2 = t * t;
             let p = Pos2::new(
-                c2 * c * self.from.x + 3.0 * c2 * t * p1.x + 3.0 * c * t2 * p2.x + t2 * t * self.to.x,
-                c2 * c * self.from.y + 3.0 * c2 * t * p1.y + 3.0 * c * t2 * p2.y + t2 * t * self.to.y
+                c2 * c * self.from.x
+                    + 3.0 * c2 * t * p1.x
+                    + 3.0 * c * t2 * p2.x
+                    + t2 * t * self.to.x,
+                c2 * c * self.from.y
+                    + 3.0 * c2 * t * p1.y
+                    + 3.0 * c * t2 * p2.y
+                    + t2 * t * self.to.y,
             );
-            painter.add(PathShape::convex_polygon(vec![
-                last,
-                p,
-                p + down,
-                last + down
-            ], Color32::LIGHT_BLUE, Stroke::new(0.5, Color32::LIGHT_BLUE)));
+            painter.add(PathShape::convex_polygon(
+                vec![last, p, p + down, last + down],
+                Color32::LIGHT_BLUE,
+                Stroke::new(0.5, Color32::LIGHT_BLUE),
+            ));
             last = p;
         }
     }
--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,11 +1,17 @@
-use std::{process::exit, net::TcpStream};
+use std::{net::TcpStream, process::exit};
 
-use electrum_client::{ElectrumApi, raw_client::RawClient, bitcoin::{Txid, hashes::hex::FromHex, Script}, Error};
-use rustls::{StreamOwned, ClientConnection};
+use electrum_client::{
+    bitcoin::{hashes::hex::FromHex, Script, Txid},
+    raw_client::RawClient,
+    ElectrumApi, Error,
+};
+use rustls::{ClientConnection, StreamOwned};
 
-pub fn get() {
-    let bitcoin = Bitcoin::new("raspibolt.local:50002").unwrap();
-    let r = bitcoin.get_transaction(&Txid::from_hex("89dea9f103d777e48c872bc9062373da7782e705386606281102d6278d01495f").unwrap()).unwrap();
+pub fn get() -> Result<(), Error> {
+    let bitcoin = Bitcoin::new("raspibolt.local:50002")?;
+    let r = bitcoin.get_transaction(&Txid::from_hex(
+        "89dea9f103d777e48c872bc9062373da7782e705386606281102d6278d01495f",
+    )?)?;
     println!("{:#?}", r);
     exit(0);
 }
@@ -14,53 +20,95 @@ pub fn get() {
 pub struct Input {
     txid: Txid,
     vout: u32,
-    value: u64
+    value: u64,
+    script: Script,
 }
 
 #[derive(Debug)]
 pub struct Output {
     value: u64,
     spend_txid: Option<Txid>,
-    script: Script
+    script: Script,
 }
 
 #[derive(Debug)]
 pub struct Transaction {
+    txid: Txid,
     inputs: Vec<Input>,
-    outputs: Vec<Output>
+    outputs: Vec<Output>,
 }
 
 pub struct Bitcoin {
-    client: RawClient<StreamOwned<ClientConnection, TcpStream>>
+    client: RawClient<StreamOwned<ClientConnection, TcpStream>>,
 }
 
 impl Bitcoin {
     pub fn new(addr: &str) -> Result<Self, Error> {
-        Ok(Self { client: RawClient::new_ssl(addr, false, None)? })
+        Ok(Self {
+            client: RawClient::new_ssl(addr, false, None)?,
+        })
     }
 
     pub fn get_transaction(self, txid: &Txid) -> Result<Transaction, Error> {
         let tx = self.client.transaction_get(txid)?;
 
-        let input_txs = self.client.batch_transaction_get(tx.input.iter().map(|i| &i.previous_output.txid))?;
-        let inputs: Vec<Input> = tx.input.iter().zip(input_txs).map(|(i, tx)| {
-            Input {
-                txid: tx.txid(),
-                vout: i.previous_output.vout,
-                value: tx.output[i.previous_output.vout as usize].value
-            }
-        }).collect();
+        let inputs: Vec<Input> = {
+            let previous_txs = self
+                .client
+                .batch_transaction_get(tx.input.iter().map(|i| &i.previous_output.txid))?;
+            tx.input
+                .iter()
+                .zip(previous_txs)
+                .map(|(input, tx)| {
+                    let vout = input.previous_output.vout;
+                    Input {
+                        txid: tx.txid(),
+                        vout,
+                        value: tx.output[vout as usize].value,
+                        script: tx.output[vout as usize].script_pubkey.clone(),
+                    }
+                })
+                .collect()
+        };
 
-        let output_histories = self.client.batch_script_get_history(tx.output.iter().map(|o| &o.script_pubkey))?;
-        let outputs = tx.output.iter().enumerate().zip(output_histories).map(|((i, o), hist)| {
-            let potential_txs = self.client.batch_transaction_get(hist.iter().map(|h| &h.tx_hash))?;
-            Ok(Output {
-                value: o.value,
-                spend_txid: potential_txs.iter().find(|p| { p.input.iter().find(|inp| inp.previous_output.txid == *txid && inp.previous_output.vout as usize == i).is_some() }).map(|t| t.txid()),
-                script: o.script_pubkey.clone()
-            })
-        }).collect::<Result<Vec<Output>, Error>>()?;
+        let outputs = {
+            let script_histories = self
+                .client
+                .batch_script_get_history(tx.output.iter().map(|o| &o.script_pubkey))?;
+            tx.output
+                .iter()
+                .enumerate()
+                .zip(script_histories)
+                .map(|((i, output), history)| {
+                    let history_txs = self
+                        .client
+                        .batch_transaction_get(history.iter().map(|h| &h.tx_hash))?;
+                    let spend_txid = history_txs
+                        .iter()
+                        .find(|history_tx| {
+                            history_tx
+                                .input
+                                .iter()
+                                .find(|history_input| {
+                                    history_input.previous_output.txid == *txid
+                                        && history_input.previous_output.vout as usize == i
+                                })
+                                .is_some()
+                        })
+                        .map(|t| t.txid());
+                    Ok(Output {
+                        value: output.value,
+                        spend_txid,
+                        script: output.script_pubkey.clone(),
+                    })
+                })
+                .collect::<Result<Vec<Output>, Error>>()?
+        };
 
-        Ok(Transaction { inputs, outputs })
+        Ok(Transaction {
+            txid: *txid,
+            inputs,
+            outputs,
+        })
     }
 }
