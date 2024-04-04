7340d7c26c8ac371054de1a77eeaef50aaee2380
Moritz Drexl <mdrexl@fastmail.fm>
2024-04-04 16:29:10 +0200

    add arrows to edges

--- src/bezier.rs
+++ src/bezier.rs
@@ -61,8 +61,7 @@ impl Edge {
             self.to + Vec2::new(0.0, self.to_height),
         );
 
-        let steps =
-            (((self.to.x - self.from.x).abs() + (self.to.y - self.from.y).abs()) / 4.0) as usize;
+        let steps = 30;
 
         let mut tops = Vec::with_capacity(steps + 1);
         let mut bots = Vec::with_capacity(steps + 1);
@@ -96,23 +95,78 @@ impl Edge {
             }
         }
 
+        let arrow_color = color.gamma_multiply(0.25);
+
         let color = if hovering {
             color.gamma_multiply(0.5)
         } else {
             color.gamma_multiply(0.4)
         };
 
+        let start = 0.3;
+        let arrow_width = 2;
+        let arrow_length = 3;
+        let start_i = (start * steps as f32) as usize;
+        let end_i = start_i + arrow_width;
+        let m_start_i = start_i + arrow_length;
+        let m_end_i = m_start_i + arrow_width;
+        let m_start = tops[m_start_i] + (bots[m_start_i] - tops[m_start_i]) / 2.;
+        let m_end = tops[m_end_i] + (bots[m_end_i] - tops[m_end_i]) / 2.;
+
         let mut mesh = Mesh::default();
-        for n in 1..=steps {
-            let i0 = (n as u32 - 1) * 4;
-            mesh.colored_vertex(tops[n - 1], color);
+        mesh.colored_vertex(tops[0], color);
+        mesh.colored_vertex(bots[0], color);
+        for n in 1..=start_i {
+            let i0 = (n as u32 - 1) * 2;
             mesh.colored_vertex(tops[n], color);
             mesh.colored_vertex(bots[n], color);
-            mesh.colored_vertex(bots[n - 1], color);
             mesh.add_triangle(i0, i0 + 1, i0 + 2);
-            mesh.add_triangle(i0, i0 + 2, i0 + 3);
+            mesh.add_triangle(i0 + 1, i0 + 2, i0 + 3);
         }
 
+        mesh.colored_vertex(m_start, color);
+
+        let i0 = mesh.vertices.len() as u32 - 1;
+        mesh.add_triangle(i0, i0 - 1, i0 - 2);
+
+        mesh.colored_vertex(m_end, color);
+        let tip_i = mesh.vertices.len() as u32 - 1;
+
+        mesh.colored_vertex(tops[end_i], color);
+        mesh.colored_vertex(bots[end_i], color);
+        for n in end_i+1..=m_end_i+1 {
+            let i0 = tip_i + (n - end_i) as u32 * 2;
+            mesh.colored_vertex(tops[n], color);
+            mesh.colored_vertex(bots[n], color);
+            mesh.add_triangle(i0, tip_i, i0 + 2);
+            mesh.add_triangle(i0 - 1, tip_i, i0 + 1);
+        }
+
+        let last = mesh.vertices.len() as u32 - 1;
+        mesh.add_triangle(tip_i, last, last - 1);
+
+        for n in m_end_i+2..=steps {
+            let i0 = last + (n - m_end_i - 2) as u32 * 2;
+            mesh.colored_vertex(tops[n], color);
+            mesh.colored_vertex(bots[n], color);
+            mesh.add_triangle(i0, i0 + 1, i0 + 2);
+            mesh.add_triangle(i0 - 1, i0, i0 + 1);
+        }
+
+        ui.painter().add(mesh);
+
+        // Arrow
+        let mut mesh = Mesh::default();
+        mesh.colored_vertex(tops[start_i], arrow_color);
+        mesh.colored_vertex(tops[end_i], arrow_color);
+        mesh.colored_vertex(m_start, arrow_color);
+        mesh.colored_vertex(m_end, arrow_color);
+        mesh.colored_vertex(bots[start_i], arrow_color);
+        mesh.colored_vertex(bots[end_i], arrow_color);
+        mesh.add_triangle(0, 1, 2);
+        mesh.add_triangle(1, 2, 3);
+        mesh.add_triangle(2, 3, 4);
+        mesh.add_triangle(3, 4, 5);
         ui.painter().add(mesh);
 
         EdgeResponse { hovering, clicked }
